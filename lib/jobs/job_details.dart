4c664223f863c6f864d528cdfd4e885c92b00796
Songheng <shkt8888@gmail.com>
2022-10-12 23:53:34 +0700

    myfile

--- lib/jobs/job_details.dart
+++ lib/jobs/job_details.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 import 'package:vhr_project/jobs/jobs_screen.dart';
 
 import '../service/global_methods.dart';
@@ -29,9 +30,9 @@ class _JobDetailsScreenState extends State<JobDetailsScreen> {
   String? jobDescription;
   String? jobTitle;
   bool? recruitment;
-  Timestamp? posteDateTimeStamp;
+  Timestamp? postedDateTimeStamp;
   Timestamp? deadlineDateTimeStamp;
-  String? posteDate;
+  String? postedDate;
   String? deadlineDate;
   String? locationCompany = '';
   String? emailCompany = '';
@@ -62,11 +63,11 @@ class _JobDetailsScreenState extends State<JobDetailsScreen> {
         emailCompany = jobDatabase.get('email');
         locationCompany = jobDatabase.get('location');
         applicants = jobDatabase.get('applicants');
-        posteDateTimeStamp = jobDatabase.get('createdAt');
+        postedDateTimeStamp = jobDatabase.get('createdAt');
         deadlineDateTimeStamp = jobDatabase.get('deadlineDateTimeStamp');
         deadlineDate = jobDatabase.get('deadlineDate');
-        var postDate = posteDateTimeStamp!.toDate();
-        posteDate = '${postDate.year}-${postDate.month}-${postDate.day}';
+        var postDate = postedDateTimeStamp!.toDate();
+        postedDate = '${postDate.year}-${postDate.month}-${postDate.day}';
       });
       var date = deadlineDateTimeStamp!.toDate();
       isDeadlineAvailable = date.isAfter(DateTime.now());
@@ -86,11 +87,11 @@ class _JobDetailsScreenState extends State<JobDetailsScreen> {
         emailCompany = jobDatabase.get('email');
         locationCompany = jobDatabase.get('location');
         applicants = jobDatabase.get('applicants');
-        posteDateTimeStamp = jobDatabase.get('createdAt');
+        postedDateTimeStamp = jobDatabase.get('createdAt');
         deadlineDateTimeStamp = jobDatabase.get('deadlineDateTimeStamp');
         deadlineDate = jobDatabase.get('deadlineDate');
-        var postDate = posteDateTimeStamp!.toDate();
-        posteDate = '${postDate.year}-${postDate.month}-${postDate.day}';
+        var postDate = postedDateTimeStamp!.toDate();
+        postedDate = '${postDate.year}-${postDate.month}-${postDate.day}';
       });
       var date = deadlineDateTimeStamp!.toDate();
       isDeadlineAvailable = date.isAfter(DateTime.now());
@@ -120,6 +121,25 @@ class _JobDetailsScreenState extends State<JobDetailsScreen> {
         ),
       ],
     );
+  }
+
+  applyForjob() {
+    final Uri params = Uri(
+      scheme: 'mailto',
+      path: emailCompany,
+      query: 'subject=Applying for $jobTitle&body=Hello, please attach Resume CV file',
+    );
+    final url = params.toString();
+    launchUrlString(url);
+  }
+
+  void addNewApplicant() async {
+    var docRef = FirebaseFirestore.instance.collection('jobs').doc(widget.jobID);
+
+    docRef.update({
+      'applicants': applicants + 1,
+    });
+    Navigator.pop(context);
   }
 
   @override
@@ -323,10 +343,173 @@ class _JobDetailsScreenState extends State<JobDetailsScreen> {
                                           color: Colors.green,
                                         ),
                                       ),
+                                      SizedBox(
+                                        width: 40,
+                                      ),
+                                      TextButton(
+                                        onPressed: () {
+                                          User? user = _auth.currentUser;
+                                          final _uid = user!.uid;
+                                          if (_uid == widget.uploadedBy) {
+                                            try {
+                                              FirebaseFirestore.instance
+                                                  .collection('jobs')
+                                                  .doc(widget.jobID)
+                                                  .update({'recruitment': false});
+                                            } catch (error) {
+                                              GlobalMethods.showErrorDialog(
+                                                error: 'Action cannot be performed',
+                                                ctx: context,
+                                              );
+                                            }
+                                          } else {
+                                            GlobalMethods.showErrorDialog(
+                                              error: 'You cannot performed this action',
+                                              ctx: context,
+                                            );
+                                          }
+                                          getJobData();
+                                        },
+                                        child: const Text(
+                                          'OFF',
+                                          style: TextStyle(
+                                            fontStyle: FontStyle.italic,
+                                            color: Colors.black,
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.normal,
+                                          ),
+                                        ),
+                                      ),
+                                      Opacity(
+                                        opacity: recruitment == false ? 1 : 0,
+                                        child: const Icon(
+                                          Icons.check_box,
+                                          color: Colors.red,
+                                        ),
+                                      ),
                                     ],
                                   ),
                                 ],
-                              )
+                              ),
+                        dividerWidget(),
+                        const Text(
+                          'Job Description',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          jobDescription == null ? '' : jobDescription!,
+                          textAlign: TextAlign.justify,
+                          style: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        dividerWidget(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Card(
+                  color: Colors.black54,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Center(
+                          child: Text(
+                            isDeadlineAvailable
+                                ? 'Actively Recruiting, Send CV/Resume:'
+                                : 'Deadline Passed away.',
+                            style: TextStyle(
+                              color: isDeadlineAvailable ? Colors.green : Colors.red,
+                              fontWeight: FontWeight.normal,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 6,
+                        ),
+                        Center(
+                          child: MaterialButton(
+                            onPressed: () {
+                              applyForjob();
+                            },
+                            color: Colors.blueAccent,
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(13),
+                            ),
+                            child: const Padding(
+                              padding: EdgeInsets.symmetric(vertical: 14),
+                              child: Text(
+                                'Apply Now',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        dividerWidget(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              'Uploaded on:',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            Text(
+                              postedDate == null ? '' : postedDate!,
+                              style: const TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 12,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              'Deadline date:',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            Text(
+                              deadlineDate == null ? '' : deadlineDate!,
+                              style: const TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ],
+                        ),
+                        dividerWidget(),
                       ],
                     ),
                   ),
