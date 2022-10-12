681bd2a1cf949453eee6c8166e439c0b99b663ed
Songheng <shkt8888@gmail.com>
2022-10-12 17:36:42 +0700

    myfile

--- lib/jobs/job_details.dart
+++ lib/jobs/job_details.dart
@@ -1,9 +1,12 @@
 import 'dart:ui';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:vhr_project/jobs/jobs_screen.dart';
+
+import '../service/global_methods.dart';
 
 class JobDetailsScreen extends StatefulWidget {
   final String uploadedBy;
@@ -18,6 +21,8 @@ class JobDetailsScreen extends StatefulWidget {
 }
 
 class _JobDetailsScreenState extends State<JobDetailsScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   String? authorName;
   String? userImageUrl;
   String? jobCategory;
@@ -68,11 +73,36 @@ class _JobDetailsScreenState extends State<JobDetailsScreen> {
     }
   }
 
+  void fetchData() async {
+    final DocumentSnapshot jobDatabase =
+        await FirebaseFirestore.instance.collection('jobs').doc(widget.jobID).get();
+    if (jobDatabase == null) {
+      return;
+    } else {
+      setState(() {
+        jobTitle = jobDatabase.get('jobTitle');
+        jobDescription = jobDatabase.get('jobDescription');
+        recruitment = jobDatabase.get('recruitment');
+        emailCompany = jobDatabase.get('email');
+        locationCompany = jobDatabase.get('location');
+        applicants = jobDatabase.get('applicants');
+        posteDateTimeStamp = jobDatabase.get('createdAt');
+        deadlineDateTimeStamp = jobDatabase.get('deadlineDateTimeStamp');
+        deadlineDate = jobDatabase.get('deadlineDate');
+        var postDate = posteDateTimeStamp!.toDate();
+        posteDate = '${postDate.year}-${postDate.month}-${postDate.day}';
+      });
+      var date = deadlineDateTimeStamp!.toDate();
+      isDeadlineAvailable = date.isAfter(DateTime.now());
+    }
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     getJobData();
+    fetchData();
   }
 
   Widget dividerWidget() {
@@ -232,6 +262,71 @@ class _JobDetailsScreenState extends State<JobDetailsScreen> {
                             ),
                           ],
                         ),
+                        FirebaseAuth.instance.currentUser!.uid != widget.uploadedBy
+                            ? Container()
+                            : Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  dividerWidget(),
+                                  const Text(
+                                    'Recruitment',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      TextButton(
+                                        onPressed: () {
+                                          User? user = _auth.currentUser;
+                                          final _uid = user!.uid;
+                                          if (_uid == widget.uploadedBy) {
+                                            try {
+                                              FirebaseFirestore.instance
+                                                  .collection('jobs')
+                                                  .doc(widget.jobID)
+                                                  .update({'recruitment': true});
+                                            } catch (error) {
+                                              GlobalMethods.showErrorDialog(
+                                                error: 'Action cannot be performed',
+                                                ctx: context,
+                                              );
+                                            }
+                                          } else {
+                                            GlobalMethods.showErrorDialog(
+                                              error: 'You cannot performed this action',
+                                              ctx: context,
+                                            );
+                                          }
+                                          getJobData();
+                                        },
+                                        child: const Text(
+                                          'OK',
+                                          style: TextStyle(
+                                            fontStyle: FontStyle.italic,
+                                            color: Colors.black,
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.normal,
+                                          ),
+                                        ),
+                                      ),
+                                      Opacity(
+                                        opacity: recruitment == true ? 1 : 0,
+                                        child: const Icon(
+                                          Icons.check_box,
+                                          color: Colors.green,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              )
                       ],
                     ),
                   ),
